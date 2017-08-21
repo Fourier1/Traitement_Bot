9398871a83cd59230ca625c646da8dfd9a4c9cd2
fourierblog <fouriersaint@gmail.com>
2017-08-21 15:33:46 +0000

    modification des paquets a installer pour demarer le programme

--- a_installer.py
+++ a_installer.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
-import os, sys
+import os
+import sys
 #
 # etre administrateur
 if not os.geteuid() == 0:
@@ -7,7 +8,7 @@ if not os.geteuid() == 0:
 
 header = """
   ------------------
-< \033[1;36mTrity Installer!!\033[1;36m >
+< \033[1;36mPaquet à Installer!!\033[1;36m >
   ------------------
          \   ^__^
           \  (oo)\_______
@@ -23,22 +24,14 @@ print("\n--------------------------")
 print("(1) Kali Linux / Ubuntu / Raspbian")
 print("--------------------------\n")
 
-option = input("\033[0m[>] Select Operating System: \033[0m")
-
-if option == "1":
-    print("\033[1;33m[*] Loading...\033[0m")
-    os.system('apt-get install python3.5')
-    os.system('easy_install pip3')
-    import pip
-    os.system('sudo apt-get install libjpeg-dev libfreetype6 zlib1g-dev')
-    os.system('pip install email')
-    os.system('pip install smtplib')
-    os.system('pip install zipfile')
-    os.system('pip install glob')
-    os.system('pip install shutil')
-    install = os.system("apt-get update && apt-get install -y build-essential git")
-    os.system('apt-get install sendemail')
-    print("\033[1;32m[!] Fin des Installations  [!]\033[0m")
-    sys.exit()
-else:
-    print ("Oups! Quelque chose s'est mal passé!")
+os.system('apt-get install python-pip')
+os.system('apt-get install python3.5')
+os.system('easy_install pip')
+import pip
+os.system('pip install email')
+os.system('pip install smtplib')
+os.system('pip install zipfile')
+os.system('pip install glob')
+os.system('pip install shutil')
+install = os.system("apt-get update && apt-get install -y build-essential git")
+pip.main(["install", "email", "zipfile", "glob", "shutil"])
