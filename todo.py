249b50950176d3f64edb51eedef282154da5b193
fourier_saint <fourier_saint@localhost.localdomain>
2017-08-30 12:29:12 +0000

    effacer les taches accomplis du fichier todo

--- todo.py
+++ todo.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 
-# todo finalise le code traitement en ajoutant le chamin des fichier
-# todo modifier l'ecriture a_intaller pour les bibliotheques a installer pour l'execution du code
-# todo ecrire le code de compression
 # todo ecrire le code telechargement sur le serveru shado
 # todo ecrire le code de decompression
